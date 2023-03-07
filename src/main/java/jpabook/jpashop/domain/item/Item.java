b25fdfed570fb86ed1e584b15a776a47a49f6cd2
yunhuiGwak <kwakyh17@naver.com>
2023-03-07 14:52:41 +0900

    :necktie: 상품 엔티티에 재고수량증감 비즈니스로직 추가 - Item 엔티티에 stockQuantity가 있으므로 관련된 재고수량증감 비즈니스로직을 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,6 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {    // 구현체를 가지고 만들거라 추상클래스로 작성
@@ -26,4 +26,26 @@ public abstract class Item {    // 구현체를 가지고 만들거라 추상클
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    //데이터를 가지고 있는 쪽에 비즈니스 로직을 넣는 것이 좋다.(객체지향적)
+    //stockQuantity가 있으므로 재고수량 관련 로직 작성
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        //남은수량 계산
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
