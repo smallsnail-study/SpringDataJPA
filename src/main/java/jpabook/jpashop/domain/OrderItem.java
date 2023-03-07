86e08cbb7e75c0fd0106ff55ca628c9df1586f05
yunhuiGwak <kwakyh17@naver.com>
2023-03-07 16:03:41 +0900

    :sparkles: 주문상품 Entity 개발 - 생성메서드createOrderItem(): 주문상품,가격,수량정보를 사용해서 주문상품엔티티를 생성. item.removeStock(count)를 호출해서 주문한수량만큼 상품의 재고를 줄인다. - 주문취소cancel(): getItem().addStock(count)를 호출해서 취소한 주문수량만큼 상품의 재고를 증가시킨다. - 주문가격조회getTotalPrice(): 주문가격에 수량을 곱한 값을 반환

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,29 @@ public class OrderItem {
 
     private int orderPrice; //주문당시의 가격
     private int count;  //주문수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);    // orderItem을 생성할 때는 기본적으로 재고수량에서 주문수량을 빼준다.
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);  //아이템을 가져와서 재고수량을 주문수량만큼 늘려줘야한다.
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {    //주문할때 주문가격과 수량을 곱해서 계산해야한다.
+        return getOrderPrice() * getCount();
+    }
 }
