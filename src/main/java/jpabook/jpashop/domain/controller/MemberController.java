a3f99a1fd4258c0582c41ed753de31e26ae9b499
yunhuiGwak <kwakyh17@naver.com>
2023-03-09 16:14:11 +0900

    :sparkles: 회원등록 controller - BindingResult로 필수입력인 회원이름 미작성 시 오류를 담아서 코드실행하도록 작성

---
+++ src/main/java/jpabook/jpashop/domain/controller/MemberController.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.domain.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new") //Get은 화면을 열어보는게 목적
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")    //Post는 데이터등록이 목적
+    public String create(@Valid MemberForm form, BindingResult result) {    //BindingResult : 오류를 담아서 코드실행가능
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
