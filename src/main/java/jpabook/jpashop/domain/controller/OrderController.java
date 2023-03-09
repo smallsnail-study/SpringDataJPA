717253ebd1f0333a8ce001e182290f23912d294d
yunhuiGwak <kwakyh17@naver.com>
2023-03-09 20:12:06 +0900

    :sparkles: 상품 주문 컨트롤러 - 메인화면에서 상품 주문을 선택하면 /order를 GET방식으로 호출 - 주문화면에서는 고객정보와 상품 정보가 필요하므로 model 객체에 담아서 뷰화면으로 넘긴다.

---
+++ src/main/java/jpabook/jpashop/domain/controller/OrderController.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.domain.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.service.ItemService;
+import jpabook.jpashop.domain.service.MemberService;
+import jpabook.jpashop.domain.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    //고객,아이템 모두 선택 가능해야함
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createform(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";  //주문내역목록으로 redirect
+    }
+}
