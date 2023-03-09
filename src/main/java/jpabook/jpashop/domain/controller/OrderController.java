999aee9d3da9f33191bcca5cfc97fd358551effa
yunhuiGwak <kwakyh17@naver.com>
2023-03-09 20:22:03 +0900

    :sparkles: 주문목록 검색 컨트롤러

--- src/main/java/jpabook/jpashop/domain/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/domain/controller/OrderController.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.repository.OrderSearch;
 import jpabook.jpashop.domain.service.ItemService;
 import jpabook.jpashop.domain.service.MemberService;
 import jpabook.jpashop.domain.service.OrderService;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,5 +45,13 @@ public class OrderController {
 
         orderService.order(memberId, itemId, count);
         return "redirect:/orders";  //주문내역목록으로 redirect
+    }
+
+    @GetMapping("/orders")  // 주문목록
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
     }
 }
