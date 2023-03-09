ac3d7345610fd1fb0e1bb82ebb2274d1ed8931ee
yunhuiGwak <kwakyh17@naver.com>
2023-03-09 16:57:44 +0900

    :sparkles: 상품등록 폼 - 상품등록 시 사용하는 폼 객체

---
+++ src/main/java/jpabook/jpashop/domain/controller/BookForm.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+
+    //상품관련 공통속성
+    private Long id;    // 상품수정을 위해 id값이 필요
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    //책만의 개별 속성
+    private String author;
+    private String isbn;
+}
