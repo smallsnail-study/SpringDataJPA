d1a59ff6fa54c79d0b5db81b93602c01132dfde5
yunhuiGwak <kwakyh17@naver.com>
2023-03-09 14:31:54 +0900

    :sparkles: 홈컨트롤러 등록

---
+++ src/main/java/jpabook/jpashop/domain/controller/HomeController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";  //home.html파일로 찾아간다.
+    }
+}
