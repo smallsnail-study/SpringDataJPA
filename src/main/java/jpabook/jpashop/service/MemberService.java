97f7160a95891523fe93bad11286563e69981bda
yunhuiGwak <kwakyh17@naver.com>
2023-03-10 17:01:41 +0900

    :sparkles: 회원수정 update - 변경 감지를 사용해서 데이터를 수정

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -48,4 +48,14 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    /**
+     * 회원 수정
+     * - 변경 감지를 사용해서 데이터를 수정
+     */
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
