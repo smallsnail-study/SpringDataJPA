35faa45a70def775d2d5600e90d44cc5588fa076
yunhuiGwak <kwakyh17@naver.com>
2023-03-13 19:35:03 +0900

    :sparkles: 엔티티직접노출 방법에서 양방향 관계 문제 발생 -> @JsonIgnore 설정

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -20,10 +20,11 @@ public class Member {
     @NotEmpty
     private String name;
 
-    @JsonIgnore // 응답값에서 제외
+
     @Embedded
     private Address address;
 
+    @JsonIgnore // 응답값에서 제외
     @OneToMany(mappedBy = "member") // 일대다관계, 하나의 회원이 여러상품을 주문, Order테이블에 있는 member에 의해서 매핑된 거울.(읽기전용),값을 넣는다고 해서 값이 변경되지 않는다.
     private List<Order> orders = new ArrayList<>();
 
