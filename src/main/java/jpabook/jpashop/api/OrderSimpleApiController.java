574157eef11f96c08545f0d2d1300f63b57c3fad
yunhuiGwak <kwakyh17@naver.com>
2023-03-13 20:13:28 +0900

    :sparkles: 간단한 주문 조회 V2: 엔티티를 DTO로 변환 - 엔티티를 DTO로 변환하는 일반적인 방법이다. - 쿼리가 총 1 + N + N번 실행된다. (v1과 쿼리수 결과는 같다.) - order 조회 1번(order 조회 결과 수가 N이 된다.) - order -> member 지연 로딩 조회 N 번 - order -> delivery 지연 로딩 조회 N 번 - 예) order의 결과가 4개면 최악의 경우 1 + 4 + 4번 실행된다(최악의 경우) - 지연로딩은 영속성 컨텍스트에서 조회하므로, 이미 조회된 경우 쿼리를 생략한다.

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne) 관계 최적화
@@ -38,5 +43,44 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();   //Lazy 강제 초기화
         }
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     * - 쿼리가 총 1 + N + N번 실행된다. (v1과 쿼리수 결과는 같다.)
+     * - order 조회 1번(order 조회 결과 수가 N이 된다.)
+     * - order -> member 지연 로딩 조회 N 번
+     * - order -> delivery 지연 로딩 조회 N 번
+     * 예) order의 결과가 4개면 최악의 경우 1 + 4 + 4번 실행된다.(최악의 경우)
+     * - 지연로딩은 영속성 컨텍스트에서 조회하므로, 이미 조회된 경우 쿼리를 생략한다.
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //ORDER 2개
+        //N + 1 -> 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;    //배송지정보(고객주소x)
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
