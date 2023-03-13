22ff16b94f5310a6c0fca8ead7b99ffdda1bd6c4
yunhuiGwak <kwakyh17@naver.com>
2023-03-13 21:25:42 +0900

    :sparkles: 간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화 - fetch join으로 쿼리 1번 호출 - 엔티티를 페치 조인(fetch join)을 사용해서 쿼리 1번에 조회 - 페치 조인으로 order -> member , order -> delivery 는 이미 조회 된 상태 이므로 지연로딩X

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -83,4 +83,21 @@ public class OrderSimpleApiController {
             address = order.getDelivery().getAddress(); //LAZY 초기화
         }
     }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함)
+     * - 엔티티를 페치 조인(fetch join)을 사용해서 쿼리 1번에 조회
+     * - 페치 조인으로 order -> member , order -> delivery 는 이미 조회 된 상태 이므로 지연로딩X
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
 }
