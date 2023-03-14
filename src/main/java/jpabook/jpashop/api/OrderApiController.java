202c58f00a020686ae4ea8dd0139c05e00ba6035
yunhuiGwak <kwakyh17@naver.com>
2023-03-14 15:47:27 +0900

    :sparkles: 컬렉션(OneToMany) 조회 최적화,주문 조회 V1: 엔티티 직접 노출 - 컬렉션인 일대다 관계(OneToMany)를 조회하고, 최적화하는 방법

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * 컬렉션 조회 최적화
+     * 주문 조회 V1: 엔티티 직접 노출
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            //강제초기화
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
